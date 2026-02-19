fca40f9676f2b59749f50fcebaada14eaf503161
VeterChul <s26i_chulinin@179.ru>
2026-02-19 20:30:36 +0300

    Написал ко все READMY

--- hypr/create_symlinks.sh
+++ hypr/create_symlinks.sh
@@ -1,6 +1,6 @@
-for d in ~/.myconfig/hypr/*/; do
+for d in hypr/*/; do
     b=$(basename "$d")
     rm -rf "$HOME/.config/$b"
 done
 
-find ~/.myconfig/hypr/ -maxdepth 1 -mindepth 1 -type d -exec ln -s -t ~/.config/ {} +
+find hypr/ -maxdepth 1 -mindepth 1 -type d -exec ln -s -t ~/.config/ {} +
--- zsh/create_symlinks.sh
+++ zsh/create_symlinks.sh
@@ -1,6 +1,8 @@
 #~/bin/bash
 
-ln -s ~/.myconfig/zsh/.zshrc ~/.zshrc
-ln -s ~/.myconfig/zsh/.p10k.zsh ~/.p10k.zsh
-ln -s ~/.myconfig/zsh/.oh-my-zsh/themes/purple.zsh-theme ~/.oh-my-zsh/themes/purple.zsh-theme 
-ln -s ~/.myconfig/zsh/.oh-my-zsh/custom ~/.oh-my-zsh/custom 
+rm ~/.zshrc
+rm -rf ~/.oh-my-zsh/custom
+
+ln -s zsh/.zshrc ~/.zshrc
+ln -s .oh-my-zsh/themes/purple.zsh-theme ~/.oh-my-zsh/themes/purple.zsh-theme 
+ln -s .oh-my-zsh/custom ~/.oh-my-zsh/custom 
